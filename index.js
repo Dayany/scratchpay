7ae3439f63aa55e3e44829676428fef521c8fe8a
Dayan Yamin <yamindayan@gmail.com>
2021-11-16 15:24:20 -0400

    Load clinics json:

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
 const cors = require("cors");
+const vetClinics = require("./data/vet-clinics.json");
+const dentalClinics = require("./data/dental-clinics.json");
+const clinics = [...vetClinics, ...dentalClinics];
 
 app.use(cors());
 
@@ -9,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  res.json({ message: "Lols" });
+  res.json({ message: clinics });
 });
 
 app.listen(port, () => console.log(`Ready at port: ${[port]}`));
